0245b1cc574fd416038bfa14138cf42d835b62a0
tech-ki <ciarra1040@gmail.com>
2025-11-11 23:15:51 -0500

    ignore my-env

--- my_env/projects/my_tennis_club/members/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Member(models.Model):
-  log = models.TextField()
-  item = models.CharField(max_length=255)
-  cclass = models.CharField(max_length=255)
